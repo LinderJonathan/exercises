5a97fcd588dc19af0d64c0d939b6be9d1739ba75
LinderJonathan <linderjonathan3@gmail.com>
2021-10-24 14:46:34 +0200

    Update: Difference in dateTime from string

---
+++ dateTimeExercises/dateTimeExercises/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dateTimeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getDayDifference();
+        }
+        /**
+         * Method calculates a number of time differences in days from fixed referenceDate
+         */
+        static void getDayDifference()
+        {
+            DateTime reference = new DateTime(2018, 03, 22);
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter date (yyyymmdd): ");
+                DateTime actual = DateTime.Parse(Console.ReadLine());
+                Console.Write("# of days from reference = " + (actual - reference).TotalDays);
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
