b6c614e9e22451e0c8d735ca1649fd7a384e8085
LinderJonathan <linderjonathan3@gmail.com>
2021-10-25 18:58:11 +0200

    Update: arrayExercises

--- arrays/arrays/Program.cs
+++ arrays/arrays/Program.cs
@@ -12,7 +12,7 @@ namespace arrays
         {
             //Console.WriteLine(SumIntArray(new int[]{1,2,3,4}));
             //uniqueElements(new int[]{ 1, 1, 2, 3, 5, 4, 5, 6, 8, 8 });
-
+            descendingOrder(new int[] { 1, 2, 5,3,9,7,6,3,1,6, 5, 6 });
             Console.Write("Type any key to exit");
             Console.ReadKey();
         }
@@ -28,6 +28,9 @@ namespace arrays
             }
             return sum;
         }
+        /**
+         * Array exercise xx. sort unique elements in list
+         */
         static void uniqueElements(int[] arr)
         {
             int ctr;
@@ -80,10 +83,55 @@ namespace arrays
                     oddLength++;
             }
 
-            int[] evenArr = new int[evenLength];
-            int[] oddArr  = new int[oddLength];
-            //evenArr = evenList.ToArray();
-            //oddArr  = oddList.ToArray();
+            int[] evenArr = (int[]) evenList.ToArray(typeof(int));
+            int[] oddArr = (int[]) oddList.ToArray(typeof(int));
+        }
+
+        /**
+         * Array exercise 12, w3resource
+         */
+        static void descendingOrder(int[] arr)
+        {
+            int tmp;
+            
+            int[] refArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+
+            }
+            for (int k = 0; k < arr.Length; k++)
+            {
+                Console.Write("{0}, ", arr[k]);
+            }
+            Console.Write("\n");
+        }
+
+        /**
+         * Array exercise 16, second largest element in array
+         */
+        static int SndLargest (int[] arr)
+        {
+            int largest = arr[0];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] >= largest)
+                {
+                    largest = arr[i];
+                }
+
+            }
+            return 0;
         }
     }
 }
