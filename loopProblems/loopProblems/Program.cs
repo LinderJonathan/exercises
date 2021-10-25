8972bcc68532838dbc40f042cba0f2cb0dcf1505
LinderJonathan <linderjonathan3@gmail.com>
2021-10-25 14:14:50 +0200

    Jag hatar detta

--- loopProblems/loopProblems/Program.cs
+++ loopProblems/loopProblems/Program.cs
@@ -11,6 +11,7 @@ namespace loopProblems
             //Console.ReadKey();
             //christmasTree();
             //DiamondPattern();
+            Console.WriteLine("Lol");
         }
 
         /**
