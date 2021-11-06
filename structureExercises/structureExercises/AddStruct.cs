6ad1b8abe8a197f80794e9f9d1051c58f932db96
LinderJonathan <linderjonathan3@gmail.com>
2021-11-06 18:49:03 +0100

    Added: Structs for exercise

---
+++ structureExercises/structureExercises/AddStruct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Struct exercise 1. Simple addition
+ */
+struct addition
+{
+    public int x;
+    public int y;
+}
+
+namespace structureExercises
+{
+    class AddStruct
+    {
+        public static void main()
+        {
+            Console.Write("\n Declare simple structure: \n");
+            Console.Write("\n ------------------------- \n");
+            addition adding = new addition();
+            adding.x = int.Parse(Console.ReadLine());
+            adding.y = int.Parse(Console.ReadLine());
+            int sum = adding.x + adding.y;
+            Console.WriteLine("Sum is: " + sum);
+
+        }
+    }
+}
---
+++ structureExercises/structureExercises/addStructStatic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+struct addStatic
+{
+    public static int x;
+    public static int y;
+}
+
+namespace structureExercises
+{
+    class addStructStatic
+    {
+        public static void main()
+        {
+            Console.Write("\n Declare simple structure: \n");
+            Console.Write("\n ------------------------- \n");
+            addition adding = new addition();
+            adding.x = int.Parse(Console.ReadLine());
+            adding.y = int.Parse(Console.ReadLine());
+            int sum = adding.x + adding.y;
+            Console.WriteLine("Sum is: " + sum);
+        }
+    }
+}
